7cda22ecdf38867b152b365a960f5a3f72d006bf
siddhipathak05 <spathak2910@gmail.com>
2022-12-09 19:06:22 +0530

    Updates

---
+++ precache-manifest.61830f2834dd39ec81b0965af3b74192.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "52b77a9baf87f23753e4",
+    "url": "/Online_Editor/static/css/main.be8f8fea.chunk.css"
+  },
+  {
+    "revision": "52b77a9baf87f23753e4",
+    "url": "/Online_Editor/static/js/main.52b77a9b.chunk.js"
+  },
+  {
+    "revision": "c625f3bb9e603565083e",
+    "url": "/Online_Editor/static/js/1.c625f3bb.chunk.js"
+  },
+  {
+    "revision": "1a05e85d3f973d301ff0",
+    "url": "/Online_Editor/static/css/2.72294e88.chunk.css"
+  },
+  {
+    "revision": "1a05e85d3f973d301ff0",
+    "url": "/Online_Editor/static/js/2.1a05e85d.chunk.js"
+  },
+  {
+    "revision": "a407438389115ebcad9c",
+    "url": "/Online_Editor/static/js/runtime~main.a4074383.js"
+  },
+  {
+    "revision": "9242fbaa42836d7b978ae45b8de973ae",
+    "url": "/Online_Editor/index.html"
+  }
+];
